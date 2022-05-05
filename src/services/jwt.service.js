09f61a3041cc0ab5fd46c26b2b9f8ae8109133a5
saurmo0108@gmail.com <saurmo0108@gmail.com>
2022-05-04 19:25:42 -0500

    Traducción español ingles

--- src/services/jwt.service.js
+++ src/services/jwt.service.js
@@ -3,33 +3,27 @@ const jwt = require('jsonwebtoken');
 const PRIVATE_KEY = "jNf21HuI8UHkyijn00ND/nmAlK0eOBCQ9uC+D9a4ybeLk/lG0nn7zjJ34U0vk3tFgbhqVQXu2ayYTfDS0CvAHQ=="
 
 /**
- * Método para generar un token
- * @param {*} data Información que guardará el token
+ * Generar un token 
+ * @param {*} payload Información que guardará el token
+ * @returns String
  */
-const crearToken = (data) => {
-    const token = jwt.sign(data, PRIVATE_KEY, { expiresIn: "5m" })
+const createToken = (payload) => {
+    const token = jwt.sign(payload, PRIVATE_KEY, { expiresIn: "5m" })
     return token
 }
 
 /**
- * 
+ * Verificar si un token es correcto
  * @param {*} token 
+ * @returns Boolean
  */
-const verificarToken = (token) => {
+const verifyToken = (token) => {
     try {
         return Object.keys(jwt.verify(token, PRIVATE_KEY)).length>0
     } catch (error) {
         return false
     }
-
 }
 
-/**
- * 
- * @param {*} token 
- */
-const decodificarToken = (token) => {
 
-}
-
-module.exports = { crearToken, verificarToken, decodificarToken }
+module.exports = { createToken, verifyToken }
