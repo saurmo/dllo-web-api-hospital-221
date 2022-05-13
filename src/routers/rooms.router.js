1562af2183f92dab8a85cfbda44dade28a93cc6c
GROOT-GEEK <ddurango712@soyudemedellin.edu.co>
2022-05-13 11:24:33 -0500

    -inicio

--- src/routers/rooms.router.js
+++ src/routers/rooms.router.js
@@ -1,1 +1,26 @@
 //PROPIEDAD DIEGO DURANGO
+// Importacion del framework de express
+const express = require('express')
+
+const router = express.Router()
+/*
+const auth_ctr = require("../controllers/auth.controller")
+const { requestValid } = require('../middlewares/token.middleware')
+const { notFound } = require('../middlewares/404.middleware')
+
+
+*/
+
+const vs = "/api/v1"
+
+router.get(`/`, (req, res)=>{
+    res.send("Proyecto de aula Hospital")
+})
+.post()
+
+router.use(requestValid)
+
+router.use(notFound)
+
+module.exports = router
+
