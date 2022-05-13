57cff89979df4370cb80ca2297034d978838125e
Juan Diego Rodríguez Guarín <minionpaul85@gmail.com>
2022-05-13 17:06:37 -0500

    Conexion MongoDB

--- config/index.config.js
+++ config/index.config.js
@@ -1,6 +1,7 @@
+process.env.URI_MONGODB= "mongodb+srv://saurmo-udem:9nVhp5fsbdKQRBLf@clusterudem.cvtno.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-process.env.URI_MONGODB= "mongodb://localhost:27017/?maxPoolSize=20&w=majority"
+process.env.DB_MONGODB="hospital-dllo-web" 
 
-process.env.DB_MONGODB="restaurante"
+process.env.COLLECTION_APPOINTMENTS_MONGODB = "appointments"
 
 process.env.PORT = 3001
