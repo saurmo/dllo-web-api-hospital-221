3e48803228624b40c3b01c0bf493a6abaee62c9f
JpRoGarcia <juan.1020405@gmail.com>
2022-05-13 15:21:29 -0500

    Creating controllers and routes

---
+++ src/routers/AppointmentsTypes.router.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const {CreateAppointmentsTypes, 
+    ReadAppointmentsTypes, 
+    ReadsAppointmentsTypes,
+    UpdateAppointmentsTypes, 
+    DeleteAppointmentsTypes
+    } = require("../controllers/AppointmentsTypes.controller");
+
+const { verificarPeticion } = require('../middlewares/token.middleware')
+
+const { notFound } = require('../middlewares/404.middleware')
+
+const router = express.Router();
+const vs = "/api/v1"
+
+// Routes
+router.get(vs + "/AppointmentsTypes", ReadsAppointmentsTypes)
+    .get(vs + "/AppointmentsTypes/:id", ReadAppointmentsTypes)
+    .post(vs + "/AppointmentsTypes", CreateAppointmentsTypes)
+    .put(vs + "/AppointmentsTypes/:id", UpdateAppointmentsTypes)
+    .delete(vs +"/AppointmentsTypes/:id", DeleteAppointmentsTypes)
+
+router.use(notFound)
+
+module.exports = router
+    
