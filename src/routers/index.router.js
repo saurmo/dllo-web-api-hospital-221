6f5b0f333e6481388af2b51465c628562acfb16f
dileyson <dem4.echeverria@gmail.com>
2022-05-13 19:42:20 -0500

    Add the clicnicHistories routes

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -3,9 +3,10 @@ const express = require('express')
 
 const router = express.Router()
 const routerConsultingRooms = require("./consultingRooms.router")
-
+const routerClinicHistories = require("./clinicHistory.router")
 
 router.use(routerConsultingRooms)
+router.use(routerClinicHistories)
 
 module.exports = router
 
