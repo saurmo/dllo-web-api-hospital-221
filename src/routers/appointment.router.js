1bc363616bdb67151e85b384bddc0458dd3dea30
pea0715 <pabloespinosarias11c@gmail.com>
2022-05-13 23:08:26 -0500

    comments routers

--- src/routers/appointment.router.js
+++ src/routers/appointment.router.js
@@ -1,11 +1,13 @@
-const express = require('express')
+// call bookstore
 const { CreateAppointments, ReadAppointments, ReadsAppointments, UpdateAppointments, DeleteAppointments } = require("../controllers/appointment.controllers");
 
 const { verificarPeticion } = require('../middlewares/token.middleware')
 
 const { notFound } = require('../middlewares/404.middleware')
 
+//instanciar routers 
 const router = express.Router();
+//instanciar routers
 const vs = "/api/v1"
 
 // Routes
@@ -16,5 +18,5 @@ router.get(vs + "/Appointments", ReadsAppointments)
     .delete(vs + "/Appointments/:id", DeleteAppointments)
 
 
-
+// export module
 module.exports = router
