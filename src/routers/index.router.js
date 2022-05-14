32c6b0730e106fa010e1d6e4fa196e0695307d35
dileyson <dem4.echeverria@gmail.com>
2022-05-13 19:15:07 -0500

    Change de file of routes

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -2,31 +2,13 @@
 const express = require('express')
 
 const router = express.Router()
-
-
-//const auth_ctr = require("../controllers/auth.controller")
-//const { requestValid } = require('../middlewares/token.middleware')
-//const { notFound } = require('../middlewares/404.middleware')
-//const consultingRooms_ctr = require("../controllers/consultingRooms.controller")
-//const clinicHistory_ctr = require("../controllers/clinicHistory.controller")
-
-const vs = "/api/v1"
-
-router.get(`/`, (req, res)=>{
-    res.send("Proyecto de aula Hospital")
-})
-
-//router.use(requestValid)
-
-router.get(vs + "/clinicHistory", clinicHistory_ctr.getLogin)
-    .put(vs + "/clinicHistory/:id", clinicHistory_ctr.updateLogin)
-    .get(vs + "/clinicHistory/:id", clinicHistory_ctr.getLogins)
-    .delete(vs + "/clinicHistory/:id", clinicHistory_ctr.deleteLogin)
-    .post(vs + "/clinicHistory", clinicHistory_ctr.createLogin)
+const routerConsultingRooms = require("./consultingRooms.router")
 
 
 
-router.use(notFound)
+
+
+router.use(routerConsultingRooms)
 
 module.exports = router
 
