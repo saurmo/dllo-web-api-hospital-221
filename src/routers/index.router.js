01aea4363511d6c1bcebeac0649fe44aa978fa64
Juan Pablo Medina <Juampamedinadiaz2002@gmail.com>
2022-05-13 19:33:18 -0500

    fix routs

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -3,10 +3,10 @@ const express = require('express')
 
 const router = express.Router()
 
-const auth_ctr = require("../controllers/auth.controller")
-const { requestValid } = require('../middlewares/token.middleware')
-const { notFound } = require('../middlewares/404.middleware')
-const clinicHistory_ctr = require("../controllers/clinicHistory.controller")
+//const auth_ctr = require("../controllers/auth.controller")
+//const { requestValid } = require('../middlewares/token.middleware')
+//const { notFound } = require('../middlewares/404.middleware')
+//const clinicHistory_ctr = require("../controllers/clinicHistory.controller")
 
 const vs = "/api/v1"
 
@@ -16,11 +16,11 @@ router.get(`/`, (req, res)=>{
 
 //router.use(requestValid)
 
-router.get(vs + "/clinicHistory", clinicHistory_ctr.getLogin)
-    .put(vs + "/clinicHistory/:id", clinicHistory_ctr.updateLogin)
-    .get(vs + "/clinicHistory/:id", clinicHistory_ctr.getLogins)
-    .delete(vs + "/clinicHistory/:id", clinicHistory_ctr.deleteLogin)
-    .post(vs + "/clinicHistory", clinicHistory_ctr.createLogin)
+//router.get(vs + "/clinicHistory", clinicHistory_ctr.getLogin)
+//    .put(vs + "/clinicHistory/:id", clinicHistory_ctr.updateLogin)
+//    .get(vs + "/clinicHistory/:id", clinicHistory_ctr.getLogins)
+//    .delete(vs + "/clinicHistory/:id", clinicHistory_ctr.deleteLogin)
+//    .post(vs + "/clinicHistory", clinicHistory_ctr.createLogin)
 
 
 router.use(notFound)
