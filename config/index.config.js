13f2b3a315bd8e30537667bd088fecd5aed05ada
saurmo0108@gmail.com <saurmo0108@gmail.com>
2022-05-18 18:50:37 -0500

    Config deploy heroku

--- config/index.config.js
+++ config/index.config.js
@@ -3,4 +3,4 @@ process.env.URI_MONGODB= "mongodb://localhost:27017/?maxPoolSize=20&w=majority"
 
 process.env.DB_MONGODB="restaurante"
 
-process.env.PORT = 3001
+process.env.PORT =  process.env.PORT || 3001
