ed63ee315a6fb76f0ea47450f160d2366974e2c4
saurmo0108@gmail.com <saurmo0108@gmail.com>
2022-05-25 18:51:19 -0500

    socket message

--- src/sockets/index.socket.js
+++ src/sockets/index.socket.js
@@ -5,12 +5,14 @@ class SocketHospital {
         this.io = io
         this.loadSocket()
         this.users=[]
+        this.messages=[]
     }
 
     loadSocket() {
         this.io.on('connection', (socket) => {
             console.log('a user connected');
             this.newUser(socket)
+            this.onMessages(socket)
         });
     }
 
@@ -18,6 +20,14 @@ class SocketHospital {
         socket.on("nuevo-usuario", (data) => {
             this.users.push(data)
             this.io.emit("usuarios-conectados", this.users )
+        })
+    }
+
+    onMessages(socket) {
+        socket.on("onMessage", (data) => {
+            console.log(data);
+            this.messages.push(data)
+            this.io.emit("pushMessages", this.messages )
         })
     }
 
